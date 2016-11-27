911eec9b84fda384f3e96a2308603e21d556035e
MakeMeBeautiful <arielmann2@gmail.com>
2016-11-27 15:31:54 +0200

    minor changes, not made by me dircetly

--- app/src/main/java/com/example/home/makemebeautiful/contacted_users/fragments/ContactedUsersPresenterFrag.java
+++ app/src/main/java/com/example/home/makemebeautiful/contacted_users/fragments/ContactedUsersPresenterFrag.java
@@ -1,5 +1,6 @@
 package com.example.home.makemebeautiful.contacted_users.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
